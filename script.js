e717ab234a0cfd83267885e8c8327cc3c8cc746b
Teoslip <tjachiet@gmail.com>
2025-06-16 22:34:42 +0200

    Add template files and base library array and objects

---
+++ script.js
@@ -0,0 +1,16 @@
+let books = [];
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error('You must have the keyword "new" to call this constructor');
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+}
+
+function storeBookInArray(Book) {
+    books.push(Book);
+}
